e6c5412140b76ed0e5628a63ed255b71186c9ed5
ysi-99999yyyy <francjulian2@gmail.com>
2024-06-10 12:31:06 +0300

    Update play.py

--- MatrixMusic/plugins/play/play.py
+++ MatrixMusic/plugins/play/play.py
@@ -30,7 +30,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(   
-              text=f"𝗕𝗟𝗔𝗖𝗞 |⌯𖤒˼ ˹🖤˼", url=f"https://t.me/KKC8C",)                        
+              text=f"قنات❤️حبيبي❤️مقاطع❤️فيديوهات❤️بنات", url=f"https://t.me/UUYUUS2",)                        
         ],        
     ]
 )
@@ -38,10 +38,10 @@ async def check_is_joined(message):
     try:
         userid = message.from_user.id
         user_name = message.from_user.first_name
-        status = await app.get_chat_member("KKC8C", userid)
+        status = await app.get_chat_member("UUYUUS2", userid)
         return True
     except Exception:
-        await message.reply_text(f'┇عزيزي: {message.from_user.mention}\n┇أشتࢪك في قناة البوت أولاً.\n┇قناة البوت: @KKC8C 🍓. ',reply_markup=force_btn,disable_web_page_preview=False)
+        await message.reply_text(f'┇عزيزي: {message.from_user.mention}\n┇أشتࢪك في قناة البوت أولاً.\n┇قناة البوت: @UUYUUS2 🍓. ',reply_markup=force_btn,disable_web_page_preview=False)
         return False
 
 
