7e9e06f1aa296d2c31ea273675d8fdc117f58625
ysi-99999yyyy <francjulian2@gmail.com>
2024-06-15 08:11:58 +0200

    Add files via upload

--- MatrixMusic/plugins/tools/must_join.py
+++ MatrixMusic/plugins/tools/must_join.py
@@ -5,27 +5,27 @@ from MatrixMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/UUYUUS2":  # Not compulsory
+    if not "https://t.me/KKC8C":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("O_0_O_w", msg.from_user.id)
+            await bot.get_chat_member("KKC8C", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/O_0_O_w".isalpha():
-                link = "https://t.me/O_0_O_w"
+            if "https://t.me/KKC8C".isalpha():
+                link = "https://t.me/KKC8C"
             else:
-                chat_info = await bot.get_chat("O_0_O_w")
+                chat_info = await bot.get_chat("kkc8c")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ حَبيبي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البوت .\n⌯︙قناة : https://t.me/O_0_O_w 🍓.\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ حَبيبي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البوت .\n⌯︙قناة : t.me/KKC8C 🍓.\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("اެختِݪاެفِـيَہِٰ 🪬♥﮼•", url=link)]
+                        [InlineKeyboardButton("𝗕𝗟𝗔𝗖𝗞 |⌯𖤒˼ ˹🖤˼", url=link)]
                     ])
-                
+                )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @O_0_O_w !")
+        print(f"I'm not admin in the MUST_JOIN chat @KKC8C !")
